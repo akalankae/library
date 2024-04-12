26b0b110c413bb37aad5e2c8175fbff40d7184dd
akalankae <akalankae@gmail.com>
2024-04-12 22:24:43 +1000

    chore: formatting changes

--- src/heap.c
+++ src/heap.c
@@ -2,7 +2,7 @@
  * File name: heap.c
  * Author: Akalanka Edirisinghe <akalankae@gmail.com>
  * Created on: 31 Mar 24
- * Last modified: 01 Apr 24 12.35 PM
+ * Last modified: 04 Apr 24 02.50 AM
  * Description: Automate the deallocation of dynamic memory reserved by the
  * program.
  ******************************************************************************/
@@ -12,44 +12,44 @@
 
 static struct heap *HEAP_LIST = NULL;
 
-void free_heap_list(void)
+void
+free_heap_list(void)
 {
-    struct heap *curr, *next;
+  struct heap *curr, *next;
 
-    for (curr = HEAP_LIST; curr != NULL; curr = next)
-    {
-        next = curr->next;
-        free(curr->addr);
-        free(curr);
-    }
+  for (curr = HEAP_LIST; curr != NULL; curr = next) {
+    next = curr->next;
+    free(curr->addr);
+    free(curr);
+  }
 }
 
 void
 append_heap_list(void *p)
 {
-    struct heap **last_node_p = &HEAP_LIST;
-    struct heap *new = malloc(sizeof(struct heap));
+  struct heap **last_node_p = &HEAP_LIST;
+  struct heap *new          = malloc(sizeof(struct heap));
 
-    if (new == NULL)
-    {
-        perror("malloc");
-        exit(1);
-    }
-    new->addr = p;
-    new->next = NULL;
+  if (new == NULL) {
+    perror("malloc");
+    exit(1);
+  }
+  new->addr = p;
+  new->next = NULL;
 
-    while (*last_node_p != NULL)
-        last_node_p = &((*last_node_p)->next);
+  while (*last_node_p != NULL)
+    last_node_p = &((*last_node_p)->next);
 
-    *last_node_p = new;
+  *last_node_p = new;
 }
 
-void *my_malloc(size_t bytes)
+void *
+my_malloc(size_t bytes)
 {
-    void *p = malloc(bytes);
+  void *p = malloc(bytes);
 
-    if (p != NULL)
-        append_heap_list(p);
+  if (p != NULL)
+    append_heap_list(p);
 
-    return p;
+  return p;
 }
